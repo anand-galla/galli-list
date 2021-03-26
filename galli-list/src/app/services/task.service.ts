23faf9af9a01d732fe76e58bf0a6d47bbf117221
Anand Gallla <gallaanand24@gmail.com>
2021-03-27 01:16:08 +0530

    updated task service

--- galli-list/src/app/services/task.service.ts
+++ galli-list/src/app/services/task.service.ts
@@ -23,25 +23,20 @@ export class TaskService {
   }
 
   getTask(identifier: string) {
-    return this.fireService.collection(this.collectionName).doc(identifier).snapshotChanges().pipe(map(item => {
-      const data = item.payload.data() as any;
-      Object.keys(data).filter((key) => data[key] instanceof Timestamp).forEach(key => data[key] = data[key].toDate())
-      data.identifier = item.payload.id;
-      return data;
-    }));
+    return this.fireService
+              .collection(this.collectionName)
+              .doc(identifier)
+              .snapshotChanges()
+              .pipe(map(item => this.processFirestoreObject(item)));
   }
 
   getTasks() {
-    return this.fireService.collection(this.collectionName)
-                .snapshotChanges()
-                .pipe(map(changes => {
-                  return changes.map(item => {
-                    const data = item.payload.doc.data() as any;
-                    Object.keys(data).filter((key) => data[key] instanceof Timestamp).forEach(key => data[key] = data[key].toDate())
-                    data.identifier = item.payload.doc.id;
-                    return data;
-                  });
-                }));
+    return this.fireService
+              .collection(this.collectionName)
+              .snapshotChanges()
+              .pipe(map(changes => {
+                return changes.map(item => this.processFirestoreObject(item));
+              }));
   }
 
   updateTask(identifier: string, task: models.Task) {
@@ -50,5 +45,12 @@ export class TaskService {
 
   removeTask(identifier: string) {
     return this.fireService.collection(this.collectionName).doc(identifier).delete();
+  }
+
+  private processFirestoreObject(object: any) {
+    const data = object.payload.doc.data() as any;
+    Object.keys(data).filter((key) => data[key] instanceof Timestamp).forEach(key => data[key] = data[key].toDate())
+    data.identifier = object.payload.doc.id;
+    return data;
   }
 }
